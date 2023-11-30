8844176523bf5f0044ec24186f6fbb473ebfc456
Ho Ngoc Giang <giaq1412@gmail.com>
2023-11-30 22:24:42 +0700

    Tạo Access Token và Refresh Token

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express'
-import User from '~/models/schemas/User.schema'
-import databaseService from '~/services/database.services'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { RegisterReqBody } from '~/models/requests/User.requests'
+
 import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
@@ -15,10 +17,9 @@ export const loginController = (req: Request, res: Response) => {
     })
 }
 
-export const registerController = async (req: Request, res: Response) => {
-    const { email, password } = req.body
+export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
     try {
-        const result = await usersService.register({ email, password })
+        const result = await usersService.register(req.body)
         return res.json({
             message: 'Register success',
             result
