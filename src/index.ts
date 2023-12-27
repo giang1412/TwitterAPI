3ae8523994d1719b2f2a321e4c70d47f9ac63945
Ho Ngoc Giang <giaq1412@gmail.com>
2023-12-28 04:45:53 +0700

    Upload file bằng Formidable

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from 'express'
 import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
 databaseService.connect()
 const app = express()
 const port = 4000
 app.use(express.json())
 
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
+
 app.use(defaultErrorHandler)
 
 app.listen(port, () => {
