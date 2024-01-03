6d5b3a90a89310db7a7cabf71ea8f871e6b1a084
Ho Ngoc Giang <giaq1412@gmail.com>
2024-01-03 13:32:49 +0700

    Xử lý Serving static file

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -69,5 +69,6 @@ export const USERS_MESSAGES = {
     OLD_PASSWORD_NOT_MATCH: 'Old password not match',
     CHANGE_PASSWORD_SUCCESS: 'Change password success',
 
-    GMAIL_NOT_VERIFIED: 'Gmail not verified'
+    GMAIL_NOT_VERIFIED: 'Gmail not verified',
+    UPLOAD_SUCCESS: 'Upload success'
 } as const
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
+import { UPLOAD_DIR } from './constants/dir'
+import staticRouter from './routes/static.routes'
 config()
 databaseService.connect()
 const app = express()
@@ -16,6 +18,8 @@ app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
 
+///app.use('/static', express.static(UPLOAD_DIR))
+app.use('/static', staticRouter)
 app.use(defaultErrorHandler)
 
 app.listen(port, () => {
