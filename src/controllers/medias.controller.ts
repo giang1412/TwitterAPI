5030f1fe9ef9ee9ee28a8cf243555080ca710e5b
Ho Ngoc Giang <giaq1412@gmail.com>
2024-01-10 18:24:49 +0700

    Streaming video

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
+import HTTP_STATUS from '~/constants/httpStatus'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
-
+import fs from 'fs'
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
     const url = await mediasService.uploadImage(req)
     return res.json({
@@ -28,11 +29,37 @@ export const uploadVideoController = async (req: Request, res: Response, next: N
         result: url
     })
 }
-export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+export const serveVideoStreamController = async (req: Request, res: Response, next: NextFunction) => {
+    const range = req.headers.range
+    if (!range) {
+        return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header')
+    }
     const { name } = req.params
-    return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
-        if (err) {
-            res.status((err as any).status).send('Not found')
-        }
-    })
+    const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name)
+    // 1MB = 10^6 bytes (Tính theo hệ 10, đây là thứ mà chúng ta hay thấy trên UI)
+    // Còn nếu tính theo hệ nhị phân thì 1MB = 2^20 bytes (1024 * 1024)
+
+    // Dung lượng video (bytes)
+    const videoSize = fs.statSync(videoPath).size
+    // DUng lượng video cho mỗi phân đoạn stream
+    const chunkSize = 10 ** 6 // 1MB
+    // Lấy giá trị byte bắt đầu từ header Range (vd: bytes=1048576-)
+    const start = Number(range.replace(/\D/g, ''))
+    // Lấy giá trị byte kết thúc, vượt quá dung lượng video thì lấy giá trị videoSize
+    const end = Math.min(start + chunkSize, videoSize - 1)
+
+    // Dung lượng thực tế cho mỗi đoạn video stream
+    // THường đây sẽ là chunkSize, ngoại trừ đoạn cuối cùng
+    const contentLength = end - start + 1
+    const mime = (await import('mime')).default
+    const contentType = mime.getType(videoPath) || 'video/*'
+    const headers = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': contentType
+    }
+    res.writeHead(HTTP_STATUS.PARTIAL_CONTENT, headers)
+    const videoSteams = fs.createReadStream(videoPath, { start, end })
+    videoSteams.pipe(res)
 }
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
-import { UPLOAD_IMAGE_DIR } from './constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from './constants/dir'
 import staticRouter from './routes/static.routes'
 config()
 databaseService.connect()
@@ -17,9 +17,10 @@ initFolder()
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
-
-///app.use('/static', express.static(UPLOAD_IMAGE_DIR))
 app.use('/static', staticRouter)
+
+app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
+
 app.use(defaultErrorHandler)
 
 app.listen(port, () => {
