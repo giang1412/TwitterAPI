48d00720028f38decd8ce4bdab6043613913f7e3
Ho Ngoc Giang <giaq1412@gmail.com>
2024-01-22 16:11:29 +0700

    Tạo middleware cho Create Tweet

--- src/routes/tweets.routes.ts
+++ src/routes/tweets.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { createTweetController } from '~/controllers/tweets.controller'
+import { createTweetValidator } from '~/middlewares/tweets.middlewares'
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 
@@ -10,6 +11,12 @@ const tweetsRouter = Router()
  * Method: POST
  * Body: TweetRequestBody
  */
-tweetsRouter.post('/', accessTokenValidator, verifiedUserValidator, wrapRequestHandler(createTweetController))
+tweetsRouter.post(
+    '/',
+    accessTokenValidator,
+    verifiedUserValidator,
+    createTweetValidator,
+    wrapRequestHandler(createTweetController)
+)
 
 export default tweetsRouter
