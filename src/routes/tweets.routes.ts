1051d76d842cf5e134858667b1388ba5d347c0e7
Ho Ngoc Giang <giaq1412@gmail.com>
2024-01-31 21:12:42 +0700

    Khai báo route get tweet và thêm twitter_circle

--- src/routes/tweets.routes.ts
+++ src/routes/tweets.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { createTweetController } from '~/controllers/tweets.controller'
-import { createTweetValidator } from '~/middlewares/tweets.middlewares'
+import { createTweetController, getTweetController } from '~/controllers/tweets.controller'
+import { createTweetValidator, tweetIdValidator } from '~/middlewares/tweets.middlewares'
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 
@@ -17,6 +17,19 @@ tweetsRouter.post(
     verifiedUserValidator,
     createTweetValidator,
     wrapRequestHandler(createTweetController)
+)
+
+/**
+ * Description: Get tweets detail
+ * path /:tweet_id
+ * Method: GET
+ */
+tweetsRouter.get(
+    '/:tweet_id',
+    accessTokenValidator,
+    verifiedUserValidator,
+    tweetIdValidator,
+    wrapRequestHandler(getTweetController)
 )
 
 export default tweetsRouter
