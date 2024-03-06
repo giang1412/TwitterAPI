dfb0951385ba8ea02c2893bdbdfbb3bdcbf50c65
Ho Ngoc Giang <giaq1412@gmail.com>
2024-03-06 21:57:42 +0700

    Gửi email khi đăng ký thành công

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -12,6 +12,7 @@ import { ErrorWithStatus } from '~/models/Error'
 import HTTP_STATUS from '~/constants/httpStatus'
 import Follower from '~/models/schemas/Follower.schema'
 import axios from 'axios'
+import { sendVerifyEmail } from '~/utils/email'
 config()
 class UsersService {
     private signAccessToken({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
@@ -115,6 +116,20 @@ class UsersService {
             new RefreshToken({ user_id: new ObjectId(user_id), token: refresh_token, iat, exp })
         )
         console.log('email_verify_token: ', email_verify_token)
+        // Flow verify email
+        // 1. Server send email to user
+        // 2. User click link in email
+        // 3. Client send request to server with email_verify_token
+        // 4. Server verify email_verify_token
+        // 5. Client receive access_token and refresh_token
+        await sendVerifyEmail(
+            payload.email,
+            'Verify your email',
+            `
+        <h1>Verify your email</h1>
+        <p>Click <a href="${process.env.CLIENT_URL}/verify-email?token=${email_verify_token}">here</a> to verify your email</p>
+     `
+        )
         return {
             access_token,
             refresh_token
