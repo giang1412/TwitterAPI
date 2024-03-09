4a1723e20a5368a3319cefa3ab3d48b6dfde9b3e
Ho Ngoc Giang <giaq1412@gmail.com>
2024-03-10 03:50:28 +0700

    Upload file lên s3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,5 +1,8 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 config()
 const s3 = new S3({
     region: process.env.AWS_REGION,
@@ -8,4 +11,23 @@ const s3 = new S3({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
     }
 })
-s3.listBuckets({}).then((data) => console.log(data))
+
+const file = fs.readFileSync(path.resolve('uploads/images/0ebc40cadf4a06161f87ffb00.jpg'))
+const parallelUploads3 = new Upload({
+    client: s3,
+    params: { Bucket: 'twitter-api-ap-southeast-1', Key: 'anh1.jpg', Body: file, ContentType: 'image/jpeg' },
+    tags: [
+        /*...*/
+    ], // optional tags
+    queueSize: 4, // optional concurrency configuration
+    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+    leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+    console.log(progress)
+})
+
+parallelUploads3.done().then((res) => {
+    console.log(res)
+})
