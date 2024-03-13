36d054f2fa82e4ac43b969ee73f7352f71927bdb
Ho Ngoc Giang <giaq1412@gmail.com>
2024-03-14 01:01:32 +0700

    Cập nhật validator cho route get conversations

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -576,3 +576,12 @@ export const isUserLoggedInValidator = (middleware: (req: Request, res: Response
         next()
     }
 }
+
+export const getConversationsValidator = validate(
+    checkSchema(
+        {
+            receiver_id: userIdSchema
+        },
+        ['params']
+    )
+)
