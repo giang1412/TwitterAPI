7118e8541d755e7641d03cb9374c30e01159be00
Ho Ngoc Giang <giaq1412@gmail.com>
2024-03-14 00:46:20 +0700

    Apply API get conversations vào client chat

--- src/index.ts
+++ src/index.ts
@@ -65,22 +65,22 @@ io.on('connection', (socket) => {
     users[user_id] = {
         socket_id: socket.id
     }
-    console.log(users)
-    socket.on('private message', async (data) => {
-        const receiver_socket_id = users[data.to]?.socket_id
+
+    socket.on('send_message', async (data) => {
+        const { receiver_id, sender_id, content } = data.payload
+        const receiver_socket_id = users[receiver_id]?.socket_id
         if (!receiver_socket_id) {
             return
         }
-        await databaseService.conversations.insertOne(
-            new Conversation({
-                sender_id: new ObjectId(data.from),
-                receiver_id: new ObjectId(data.to),
-                content: data.content
-            })
-        )
-        socket.to(receiver_socket_id).emit('receive private message', {
-            content: data.content,
-            from: user_id
+        const conversation = new Conversation({
+            sender_id: new ObjectId(sender_id),
+            receiver_id: new ObjectId(receiver_id),
+            content: content
+        })
+        const result = await databaseService.conversations.insertOne(conversation)
+        conversation._id = result.insertedId
+        socket.to(receiver_socket_id).emit('receive_message', {
+            payload: conversation
         })
     })
     socket.on('disconnect', () => {
