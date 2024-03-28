122b286f7fce3b4e55ba336266b3437935b0043d
Ho Ngoc Giang <giaq1412@gmail.com>
2024-03-28 17:13:06 +0700

    Sử dụng NODE_ENV và PM2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const path = require('path')
+// eslint-disable-next-line no-undef
+module.exports = {
+    apps: [
+        {
+            name: 'twitter',
+            script: path.resolve('dist', 'index.js'),
+            env: {
+                NODE_ENV: 'development', // Riêng NODE_ENV thì có thể dùng process.env.NODE_ENV hoặc process.NODE_ENV, còn lại thì chỉ được dùng process.env.TEN_BIEN
+                TEN_BIEN: 'Gia tri'
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+}
